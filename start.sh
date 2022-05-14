14a3fc2577d7b23b4b581e91040ace728734690e
Juan C. Andreu <andreujuanc@gmail.com>
2022-05-14 13:25:43 +0200

    cleanup

--- start.sh
+++
@@ -1,9 +0,0 @@
-docker run -it \
-    -v /home/user/data:/nethermind/data -p 8545:8545 \
-    nethermind/nethermind \
-    --JsonRpc.Enabled true \
-    --JsonRpc.Host 0.0.0.0 \
-    --config xdai \
-    --Network.MaxActivePeers=256 \ # this does not work
-    #--Metrics.Enabled true \ # https://docs.nethermind.io/nethermind/ethereum-client/metrics/setting-up-local-metrics-infrastracture
-    --datadir data 
